04b07c61ee25665b245d65861878aa68a302c0e4
mhmd-salah <ms.mhmdsalah@gmail.com>
2024-06-29 02:05:50 +0200

    pending

---
+++ primitve.ts
@@ -0,0 +1,10 @@
+enum Levels {
+  Kids=10,
+  Easy=30,
+  Mediam =40,
+  Hard = 1
+}
+console.log(Levels.Easy);
+
+let mimage  = document.getElementById("img") as HTMLInputElement
+console.log(mimage.src)
---
+++ rec.ts
@@ -0,0 +1,23 @@
+// object types 
+
+type st = string;
+let uesrName:string = "mohamed"
+
+type opj = {
+  user:string,
+  age:number
+}
+let user:opj={
+  user:"mhmd-salah",
+  age:21
+}
+interface Ius {
+  user:string,
+  age:number,
+  id:number,
+}
+let us :Ius={
+  user:"salah",
+  age:12,
+  id:101
+}
